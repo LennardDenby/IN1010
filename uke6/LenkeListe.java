231c7ad61515b35134f742939bd4b3e312badc2d
lennarddenby <lennard.rolstad.denby@gmail.com>
2025-03-02 22:56:52 +0100

    fikset navn og la til ressurser for uke 6

---
+++ uke6/LenkeListe.java
@@ -0,0 +1,22 @@
+public class LenkeListe {
+    private Node forste;
+    private Node siste;
+
+    private class Node {
+        //TODO:
+    }
+    public int hent() {
+        //TODO:
+    }
+    public void leggTil() {
+        //TODO:
+    }
+    public void fjern() {
+        //TODO:
+
+    }
+    @Override
+    public String toString() {
+        //TODO:
+    }
+}
